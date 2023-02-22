d905165208c983dd560353303d843185686952c0
alekelbar <alekelbar.personal@gmail.com>
2023-02-22 10:26:59 -0600

    feat(UI): Cards to show careers

---
+++ src/components/Career/CareerCard.tsx
@@ -0,0 +1,23 @@
+import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
+import React from 'react';
+
+interface CareerCardProps {
+  title: string;
+}
+
+export const CareerCard: React.FC<CareerCardProps> = ({ title }) => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {title}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          onClick={() => console.log('ver cursos')}
+        > Ver cursos </Button>
+      </CardActions>
+    </Card>
+  );
+};
