6d4b0e164cabe9cc51c4e6a7c7ff0d2e6a6eca10
alekelbar <alekelbar.personal@gmail.com>
2023-02-22 18:09:14 -0600

    feat(Careers): Float button

--- src/components/Career/AddCareerButton.tsx
+++ src/components/Career/AddCareerButton.tsx
@@ -1,25 +1,15 @@
 import { Box } from '@mui/system';
-import { SpeedDial, SpeedDialAction } from '@mui/material';
-import { Add, Edit } from '@mui/icons-material';
+import { Fab } from '@mui/material';
+import { Add } from '@mui/icons-material';
 
 interface AddCareerButtonProps {
   onAdd: () => void;
 }
 
 export const AddCareerButton: React.FC<AddCareerButtonProps> = ({ onAdd }) => (
-  <Box>
-    <SpeedDial
-      sx={{ position: 'absolute', bottom: 16, right: 10 }}
-      ariaLabel="Agregar carrera"
-      icon={<Edit />}
-      FabProps={{ sx: { width: { sm: '.4em' }, height: { sm: '80px' } } }}
-    >
-      <SpeedDialAction
-        icon={<Add sx={{ bgcolor: 'primary' }} />}
-        sx={{ mb: { md: '1em' }, fontSize: '1em', bgcolor: ({ palette }) => palette.primary.main }}
-        tooltipTitle={'Agregar una carrera'}
-        onClick={onAdd}
-      />
-    </SpeedDial>
+  <Box sx={{ position: 'absolute', bottom: 16, right: 16 }}>
+    <Fab onClick={onAdd} sx={{ width: { md: '6em' }, height: { md: '6em' } }}>
+      <Add sx={{ fontSize: { md: '2.5em' } }} />
+    </Fab>
   </Box>
 );
--- src/components/common/Copyright.tsx
+++ src/components/common/Copyright.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import MuiLink from '@mui/material/Link';
+import { Box } from '@mui/system';
 
 export default function Copyright () {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <MuiLink color="inherit" href="https://www.instagram.com/alekelbar/" target={'_blank'}>
-        alekelbar
-      </MuiLink>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
+    <Box component={'div'} sx={{ position: 'absolute', bottom: '2%', left: '2%' }}>
+      <Typography variant="body2" color="text.secondary" align="center">
+        {'Copyright © '}
+        <MuiLink color="inherit" href="https://www.instagram.com/alekelbar/" target={'_blank'}>
+          alekelbar
+        </MuiLink>{' '}
+        {new Date().getFullYear()}.
+      </Typography>
+    </Box>
   );
 }
