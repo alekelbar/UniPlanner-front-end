366e8acf297abeaa417d521b9686c07017164883
alekelbar <alekelbar.personal@gmail.com>
2023-03-05 18:48:51 -0600

    feat: using API version 2

--- src/services/api-version.ts
+++ src/services/api-version.ts
@@ -1,3 +1,3 @@
 type AvailableVersions = "v1" | "v2";
 
-export const API_VERSION: AvailableVersions = "v1";
+export const API_VERSION: AvailableVersions = "v2";
--- src/services/auth/validate-token.ts
+++ src/services/auth/validate-token.ts
@@ -1,13 +1,17 @@
 import axios from "axios";
+import { API_VERSION } from "../api-version";
 
 export const validateToken = async (token: string | null) => {
   if (token) {
     try {
-      await axios.get("http://localhost:3000/api/v1/auth/validate", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      await axios.get(
+        `http://localhost:3000/api/${API_VERSION}/auth/validate`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       return true;
     } catch (error) {}
   }
