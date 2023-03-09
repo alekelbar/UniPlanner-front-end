f120d36426084485cbccefbae15d582ccbd1a0a0
alekelbar <alekelbar.personal@gmail.com>
2023-03-09 16:53:53 -0600

    feat: Production Env

--- src/services/api-version.ts
+++ src/services/api-version.ts
@@ -2,6 +2,7 @@ type AvailableVersions = "v1" | "v2";
 
 export const API_VERSION: AvailableVersions = "v2";
 
-export const API_URL = process.env.NEXT_PUBLIC_VERCEL_ENV
-  ? `https://ge-back.onrender.com/api/${API_VERSION}/`
-  : `http://localhost:3000/api/${API_VERSION}/`;
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? `https://ge-back.onrender.com/api/${API_VERSION}/`
+    : `http://localhost:3000/api/${API_VERSION}/`;
