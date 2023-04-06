5a570f5a5a26e77f57d83715ff6d0c221044eecb
alekelbar <alekelbar.personal@gmail.com>
2023-04-06 17:15:48 -0600

    feat: form created

--- pages/home/kanban.tsx
+++ pages/home/kanban.tsx
@@ -1,8 +1,13 @@
-import { Box, Container, List, ListItem, ListItemText, Paper, Stack, Typography } from '@mui/material';
+import { Button, Container, Dialog, DialogContent, DialogTitle, List, ListItem, ListItemText, Paper, Stack, TextField, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { useState } from 'react';
 import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
 import { FloatButton } from '../../src/components';
 import { Add } from '@mui/icons-material';
+import { useAppDispatch } from '../../src/redux';
+import { useRouter } from 'next/router';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { v1 as uuidV1 } from 'uuid';
 
 // model ...
 interface TaskModel {
@@ -92,11 +97,125 @@ type ListsState = {
   DONE: TaskModel[],
 };
 
+interface KanbanAddProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+const KanbanAddTodo = ({ onClose, open }: KanbanAddProps): JSX.Element => {
+
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const width = fullScreen ? '100%' : '50%';
+
+
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      content: ''
+    },
+    onSubmit: async (values) => {
+      const { content, title } = values;
+      const task: TaskModel = {
+        id: uuidV1(),
+        content,
+        title
+      };
+
+      console.log(task);
+    },
+    validationSchema: Yup.object({
+      content: Yup
+        .string()
+        .required("La descripción de la tarea es requerida")
+        .min(10, "Trate de usar al menos 10 caracteres")
+        .max(30, "No puede usar más de 20 caracteres"),
+      title: Yup
+        .string()
+        .required("El nombre de la tarea es requerida")
+        .min(5, "Trate de usar al menos 5 caracteres")
+        .max(30, "No puede usar más de 20 caracteres"),
+    }),
+  });
+
+  return (
+    <Dialog
+      sx={{
+        '& .MuiDialog-paper': {
+          width: width,
+          height: 'auto'
+        }
+      }}
+      onClose={onClose}
+      open={open}>
+      <DialogTitle title='Nueva Tarea' />
+
+      <DialogContent>
+        <Stack
+          component={'form'}
+          onSubmit={formik.handleSubmit}
+          direction="column"
+          justifyContent={'center'}
+          alignItems={'center'}
+          spacing={2}>
+
+          <TextField
+            fullWidth
+            name="title"
+            onChange={formik.handleChange}
+            value={formik.values.title}
+            type={'text'}
+            onBlur={formik.handleBlur}
+            autoComplete="off"
+            rows={2}
+            multiline
+            placeholder="Nombre"
+            helperText="¿Como va a nombrar a esta tarea?" />
+
+          {formik.touched.title && formik.errors.title && (
+            <Typography variant='caption' color={'error'}>{formik.errors.title}</Typography>
+          )}
+
+          <TextField
+            fullWidth
+            name="content"
+            onChange={formik.handleChange}
+            value={formik.values.content}
+            type={'text'}
+            onBlur={formik.handleBlur}
+            autoComplete="off"
+            rows={2}
+            multiline
+            placeholder="descripción"
+            helperText="¿Como va a nombrar a esta tarea?" />
+
+          {formik.touched.content && formik.errors.content && (
+            <Typography variant='caption' color={'error'}>{formik.errors.content}</Typography>
+          )}
+          <Button
+            fullWidth
+            type='submit'
+            color='success'
+            variant='contained'>
+            Crear
+          </Button>
+        </Stack>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
 const Kanban = () => {
 
   const [OpenAdd, setOpenAdd] = useState(false);
   const onOpen = () => {
     setOpenAdd(true);
+  };
+
+  const onClose = () => {
+    setOpenAdd(false);
   };
 
   const [lists, setLists] = useState<ListsState>({
@@ -161,6 +280,7 @@ const Kanban = () => {
         onAction={onOpen}
         icon={<Add sx={{ fontSize: { md: '2.5em' } }} />}
         sxProps={{ position: 'fixed', bottom: 16, right: 16 }} />
+      <KanbanAddTodo onClose={onClose} open={OpenAdd} />
     </Container>
   );
 };
