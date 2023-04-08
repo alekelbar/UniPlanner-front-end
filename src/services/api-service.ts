66be4c8500088e730e9c36e0c9085e8417ef952f
alekelbar <alekelbar.personal@gmail.com>
2023-04-08 11:35:21 -0600

    chore: API using node_env

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -3,6 +3,9 @@ type AvailableVersions = "v1" | "v2";
 export const API_VERSION: AvailableVersions = "v2";
 
 // export const API_URL = `https://ge-back.onrender.com/api/${API_VERSION}/`
-export const API_URL = `http://localhost:3000/api/${API_VERSION}/`;
+export const API_URL =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:3000/api/${API_VERSION}/`
+    : `https://ge-back.onrender.com/api/${API_VERSION}/`;
 
-
+console.log(API_URL);
