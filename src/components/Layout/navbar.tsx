a4b65da6eeb227a11ae768104d7f517c5a3e1d84
alekelbar <alekelbar.personal@gmail.com>
2023-04-19 21:58:52 -0600

    chore: navbar UI updated

--- src/components/Layout/navbar.tsx
+++ src/components/Layout/navbar.tsx
@@ -1,5 +1,5 @@
-import { School } from '@mui/icons-material';
-import { AppBar, Typography } from '@mui/material';
+import { Menu } from '@mui/icons-material';
+import { AppBar, Button, Stack, Typography } from '@mui/material';
 
 interface NabvarProps {
   onOpen: () => void;
@@ -9,20 +9,23 @@ export function Navbar ({ onOpen }: NabvarProps): JSX.Element {
 
   return (
     <AppBar position='static' sx={{ p: 2 }}>
-      <Typography
-        variant="h4"
-        sx={{
-          cursor: 'pointer',
-          transition: 'all 0.3s',
-          '&:hover': {
-            transform: 'scale(1.1)',
-          },
-        }}
-        onClick={onOpen}
-        m='0 auto'>
-        <School sx={{ fontSize: '.8em' }} />
-        UniPlanner
-      </Typography>
+      <Stack direction={'row'} justifyContent={'start'} alignItems={'center'} gap={1.5}>
+        <Button
+          sx={{
+            cursor: 'pointer',
+            transition: 'all 0.3s',
+            '&:hover': {
+              transform: 'scale(1.1)',
+            },
+          }}
+          onClick={onOpen}
+        >
+          <Menu sx={{ fontSize: '2em' }} />
+        </Button>
+        <Typography variant="h4">
+          UniPlanner
+        </Typography>
+      </Stack>
     </AppBar>
   );
 }
